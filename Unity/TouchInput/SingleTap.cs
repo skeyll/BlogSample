edf916cdc3f7ddf6921424ea2bae54ddc91f5b49
Skeyll <50002207+skeyll@users.noreply.github.com>
2020-05-21 10:07:55 +0900

    Create SingleTap.cs
    First commit

---
+++ Unity/TouchInput/SingleTap.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+public class SingleTap : MonoBehaviour {
+  public void PressButton(){
+     Debug.Log("Punch");
+  }
+
+  public void ReleaseButton(){
+     Debug.Log("Kick");
+  }
+}
